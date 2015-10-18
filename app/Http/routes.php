3444d1da8733eb3ba4272090d635d5b97d2bed0d
Alex Pekhota <pekhota.alex@gmail.com>
2015-10-18 12:25:43 +0300

    first commit

---
+++ app/Http/routes.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+$app->get('/', function () use ($app) {
+    return view('wellcome');
+});
+
+function getValidTypeFromSql($type) {
+    $type = strtolower($type);
+
+    $a = [
+        'varchar' => 'string',
+        'timestamp' => 'string',
+        'int' => 'integer'
+    ];
+
+    if(isset($a[$type])) {
+        return $a[$type];
+    }
+
+    return $type;
+}
+
+$app->post('/', function (\Illuminate\Http\Request $request) use ($app) {
+    $sqlcode = $request->input('sqlcode');
+    $parsed = '';
+    if($sqlcode !== null) {
+        $parser  = new \PHPSQLParser\PHPSQLParser($sqlcode);
+        if(isset($parser->parsed['TABLE']['create-def']['base_expr'])) {
+            $p = $parser->parsed['TABLE']['create-def']['base_expr'];
+
+            preg_match_all('/`(\w+)`\s+(\w+)/',$p, $matches);
+
+            $p = array_combine($matches[1],$matches[2]);
+            $c = [];
+            foreach($p as $k => $v) {
+                $c[] = new \Saxulum\PhpDocGenerator\PropertyRow(getValidTypeFromSql($v), $k);
+
+            }
+            $document = new \Saxulum\PhpDocGenerator\Documentor($c);
+            $parsed = ((string)$document);
+        }
+    }
+    return view('wellcome', [
+        'sqlcode' => $sqlcode,
+        'parsed' => $parsed
+    ]);
+});
---
+++ resources/views/wellcome.blade.php
@@ -0,0 +1,17 @@
+@extends('layout')
+
+@section('content')
+    <div class="starter-template">
+        <h1>Sql to PHPDoc</h1>
+        <p class="lead">Вставь текст в форму ниже, нажми на кнопку и радуйся готовому phpdoc'у для твоей модели</p>
+        <form method="post">
+            <div class="form-group">
+                <label for="sqlBlockId">Sql для парсинга</label>
+                <textarea id="sqlBlockId" name="sqlcode" class="form-control" rows="3">{{$sqlcode}}</textarea>
+            </div>
+
+            <pre style="text-align: left">{{ $parsed }}</pre>
+            <button type="submit" class="btn btn-default">Парсить</button>
+        </form>
+    </div>
+@endsection
