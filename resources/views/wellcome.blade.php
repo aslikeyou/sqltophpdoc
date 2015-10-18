90117aae124015599c80542174080a3dab8e36ca
Alex Pekhota <pekhota.alex@gmail.com>
2015-10-18 14:14:27 +0300

    Fix notice3

--- resources/views/wellcome.blade.php
+++ resources/views/wellcome.blade.php
@@ -7,14 +7,14 @@
         <form method="post">
             <div class="form-group">
                 <label for="sqlBlockId">Sql (CREATE TABLE) для парсинга</label>
-                <textarea id="sqlBlockId" name="sqlcode" class="form-control" rows="3" placeholder="CREATE TABLE `news` (
+                <textarea id="sqlBlockId" name="sqlcode" class="form-control" rows="3">{{ isset($sqlcode) && !empty($sqlcode) ? $sqlcode : "CREATE TABLE `news` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `source_id` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;">{{ $sqlcode or ''}}</textarea>
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;"}}</textarea>
             </div>
 
 
