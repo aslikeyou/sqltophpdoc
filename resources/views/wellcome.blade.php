2fcfb5e6ac35a201a8b2f30c2ea102a9e2be8465
Alex Pekhota <pekhota.alex@gmail.com>
2015-10-18 13:03:46 +0300

    add placeholder

--- resources/views/wellcome.blade.php
+++ resources/views/wellcome.blade.php
@@ -6,8 +6,15 @@
         <p class="lead">Вставь текст в форму ниже, нажми на кнопку и радуйся готовому phpdoc'у для твоей модели</p>
         <form method="post">
             <div class="form-group">
-                <label for="sqlBlockId">Sql для парсинга</label>
-                <textarea id="sqlBlockId" name="sqlcode" class="form-control" rows="3">{{ $sqlcode or ''}}</textarea>
+                <label for="sqlBlockId">Sql (CREATE TABLE) для парсинга</label>
+                <textarea id="sqlBlockId" name="sqlcode" class="form-control" rows="3" placeholder="CREATE TABLE `news` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `source_id` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;">{{ $sqlcode or ''}}</textarea>
             </div>
 
 
